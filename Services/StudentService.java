5f36d48a7942689a6b7b4d6d2dea0b4d67fc9b35
CompanyProfile <alanood94895@gmail.com>
2026-05-03 10:22:53 +0400

    add studentService code

--- Services/StudentService.java
+++ Services/StudentService.java
@@ -1,5 +1,6 @@
 package ObjectOrientedProgramming.OOPDemo.Services;
 
+import ObjectOrientedProgramming.OOPDemo.Entities.Course;
 import ObjectOrientedProgramming.OOPDemo.Entities.Department;
 import ObjectOrientedProgramming.OOPDemo.Entities.Student;
 import ObjectOrientedProgramming.OOPDemo.Utils.Constants;
@@ -71,6 +72,88 @@ public class StudentService {
         modifyStudent(name, updatedStudent);
         return updatedStudent;
     }
+    public Boolean modifyStudent(String studentName, Student updatedStudent) {
+        Student existingStudent = findStudentByName(StudentName);
+
+        if (!existingStudent.getId().equals(null)) {
+            existingStudent.setName(updatedStudent.getName());
+            existingStudent .setOfferedCourses(updatedStudent.getOfferedCourses());
+            System.out.println(Constants.STUDENT_UPDATED_SUCCESSFULLY);
+            return true;
+        }
+
+        System.out.println(Constants.STUDENT_UPDATED_FAILED);
+        return false;
+    }
+
+    public Boolean deleteDepartment(){
+        System.out.println("Enter Department to Remove: ");
+        Scanner scanner = new Scanner(System.in);
+        Department departmentToRemove = findDepartmentByName(scanner.nextLine());
+        Boolean status = getDepartments().remove(departmentToRemove);
+        System.out.println(status.equals(true) ? Constants.DEPARTMENT_DELETED_SUCCESSFULLY : Constants.DEPARTMENT_DELETED_FAILED);
+        return status;
+
+
+    }
+
+    public Department findDepartmentByName(String departmentName) {
+        for(Department d : getDepartments()){
+            if(d.getName().equalsIgnoreCase(departmentName)){
+                return d;
+            }
+        }
+        return null;
+    }
+
+    public void displayDepartmentByName(){
+        System.out.println("Enter Department Name: ");
+        Scanner scanner = new Scanner(System.in);
+        Department department = findDepartmentByName(scanner.nextLine());
+        System.out.println("University Name" + UniversityService.university.getName());
+        System.out.println("Department Id: " + department.getId());
+        System.out.println("Department Name: " + department.getName());
+        for (Course c : department.getOfferedCourses()) {
+            System.out.println("Course Id: " + c.getId());
+            System.out.println("Course Name: " + c.getName());
+            System.out.println("Course Code: " + c.getCourseCode());
+        }
+    }
+
+
+
+
+    public Boolean handleDepartmentMenu(Integer departmentOption){
+        switch (departmentOption){
+            case 1-> {
+                addNewDepartments();
+            }
+            case 2->{
+                updateDepartment();
+            }
+            case 3 ->{
+                System.out.println("Show Department ");
+                displayDepartmentByName();
+            }
+
+            case 4-> {
+                deleteDepartment();
+                System.out.println("Show Departments");
+                university.displayDepartments();
+            }
+
+            case 5->{
+                System.out.println(" Show Departments ");
+                university.displayDepartments();
+            }
+            case 6 ->{
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
 
 }
 
